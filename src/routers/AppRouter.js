0c69a970372a6ada89a91d99de624a8653ba0012
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-27 17:26:40 -0300

    se agregaron 2 rutas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { JournalScreen } from '../components/journal/JournalScreen'
+import { AuthRouter } from './AuthRouter'
 
 export const AppRouter = () => {
     return (
-        <div>AppRouter</div>
+
+        <Routes>
+            <Route path='/auth' element={<AuthRouter />} />
+            <Route path='/' element={<JournalScreen />} />
+        </Routes>
+
     )
 }
