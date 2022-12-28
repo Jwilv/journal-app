06733da0fea652e5d74793a096e0440818ee2236
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-28 00:07:39 -0300

    creacion de componente en RegisterScreen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
         <div className='aut__main'>
             <div className='auth__box-container'>
-                <h1>RegisterScreen</h1>
+                <>
+                    <h3 className='auth__title mb-5'>Register</h3>
+                    <form>
+                        <input
+                            type={'text'}
+                            autoComplete={'off'}
+                            placeholder={'Name'}
+                            name={'name'}
+                            className={'auth__input'}
+                        />
+                        <input
+                            type={'text'}
+                            autoComplete={'off'}
+                            placeholder={'Email'}
+                            name={'email'}
+                            className={'auth__input'}
+                        />
+                        <input
+                            type={'password'}
+                            autoComplete={'off'}
+                            placeholder={'Password'}
+                            name={'password'}
+                            className={'auth__input'}
+                        />
+                        <input
+                            type={'password'}
+                            autoComplete={'off'}
+                            placeholder={'Confirm password'}
+                            name={'confirm'}
+                            className={'auth__input'}
+                        />
+                        <button
+                            className='btn btn-primary btn-block mb-5'
+                            type='submit'
+                        >
+                            Register
+                        </button>
+                        <Link
+                            to={'/login'}
+                            className='link'
+                        >
+                            Alredy registered?
+                        </Link>
+                    </form>
+                </>
             </div>
         </div>
     )
