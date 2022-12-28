09084b16bade8b9be237db4a6f9d243004e1f24f
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-28 17:15:34 -0300

    se agrego img

--- src/components/journal/JournaEntry.js
+++ src/components/journal/JournaEntry.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const JournaEntry = () => {
     return (
-        <div>JournaEntry</div>
+        <div className='journa__entry'>
+            <div
+                className='journa__entry-picture'
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000)',
+            }}
+            ></div>
+        </div >
     )
 }
