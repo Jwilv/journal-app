ca1867d26413d3e3f26cf366840f4065e46c2a91
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-29 17:48:33 -0300

    notesScreen base

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,20 @@ export const NoteScreen = () => {
     return (
         <div className='note__main-content'>
             <NotesAppbar />
+            <div className='note__content'>
+                <input
+                    type={'text'}
+                    placeholder={'Title'}
+                    className={'note__title-input'}
+                />
+                <textarea
+                placeholder='What happened today ?'
+                className={'note__textarea'}></textarea>
+                <div className='notes__images'>
+                    <img src='https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg'
+                    alt='img'/>
+                </div>
+            </div>
 
         </div>
     )
