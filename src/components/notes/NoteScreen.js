cabb1e739a380df03d063766dfd34fbcecda7b64
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-28 21:48:43 -0300

    NotesAppbar cerado

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { NotesAppbar } from './NotesAppbar'
 
 export const NoteScreen = () => {
     return (
         <div className='note__main-content'>
+            <NotesAppbar />
 
         </div>
     )
---
+++ src/components/notes/NotesAppbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const NotesAppbar = () => {
+    return (
+        <div className='notes__appbar'>
+            <span>otra vez en primera van a ver a papa</span>
+            <div>
+                <button className='btn'>
+                    picture
+                </button>
+                <button className='btn'>
+                    save
+                </button>
+            </div>
+        </div>
+    )
+}
