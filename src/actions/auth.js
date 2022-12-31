9498263c42128505828bef5a4f667a9fdef35d9e
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-31 16:37:33 -0300

    se agrego return

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,9 +1,11 @@
 import { types } from "../type/types";
 
 export const startLoginEmailPassword = (Email, Password) => {
-    setTimeout(() => {
-        dispatch(login(123, 'juancete'))
-    }, 3500)
+    return (dispatch) => {
+        setTimeout(() => {
+            dispatch(login(123, 'juancete'))
+        }, 3500)
+    }
 }
 
 export const login = (uid, displayName) => ({
