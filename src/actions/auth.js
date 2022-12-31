f27083701087bf73eed8a71a1c8ed9c3d36c34d3
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2022-12-31 17:37:25 -0300

    se creo startGoogleLogin

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import { firebase, googleAuthProvider } from "../firebase/firebase-config";
 import { types } from "../type/types";
 
 export const startLoginEmailPassword = (Email, Password) => {
@@ -5,6 +6,15 @@ export const startLoginEmailPassword = (Email, Password) => {
         setTimeout(() => {
             dispatch(login(Email, Password))
         }, 3500)
+    }
+}
+
+export const startGoogleLogin = () => {
+    return (dispatch) => {
+        firebase.auth().singInWithPopup( googleAuthProvider)
+        .then( userCred =>{
+            console.log(userCred)
+        })
     }
 }
 
