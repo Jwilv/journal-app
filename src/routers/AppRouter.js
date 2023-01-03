9efe203943cc69dbd6b55474b903fa994f612340
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-03 18:03:28 -0300

    se agrego el onAuthStateChanged con useEfectt

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
+import { login } from '../actions/auth'
 import { LoginScreen } from '../components/auth/LoginScreen'
 import { RegisterScreen } from '../components/auth/RegisterScreen'
 import { JournalScreen } from '../components/journal/JournalScreen'
+import { auth } from '../firebase/firebase-config'
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            console.log(user)
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName))
+            }
+        })
+    }, [])
+
     return (
 
         <Routes>
