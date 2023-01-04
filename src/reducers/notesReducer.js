e7b4d7fa69723a71aeddd4331a2f43d9bfc85d12
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-04 19:03:13 -0300

    se creo notesReducer

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,16 @@
+
+const initialState = {
+    notes: [],
+    active: null,
+}
+
+export const noteReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+
+
+        default:
+            return state;
+    }
+
+}
