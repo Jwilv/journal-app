04ec156eb8a934bb57bb13b2e58d7026e1569798
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-03 22:05:45 -0300

    se agregaron imports

--- src/routers/PublicRouter.js
+++ src/routers/PublicRouter.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 export const PublicRouter = ({ children }) => {
 
