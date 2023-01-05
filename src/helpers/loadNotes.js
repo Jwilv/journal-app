0853da2b978c65347a4c458946966a2d6ca91992
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-05 16:57:05 -0300

    se agrego parametro

--- src/helpers/loadNotes.js
+++ src/helpers/loadNotes.js
@@ -3,7 +3,7 @@ import { collection, getDoc } from "firebase/firestore"
 import { db } from "../firebase/firebase-config"
 
 
-export const loadNotes = async()=>{
+export const loadNotes = async(uid)=>{
     const docRef = collection(db,`${uid}`, 'journal', 'notes');
     const notesSnap = await getDoc(docRef);
     console.log(notesSnap);
