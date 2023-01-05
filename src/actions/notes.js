394896d0ede8f92f5eeaa4801698ff77ae16dea0
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-05 00:15:53 -0300

    se crea una nueva nota en la base de datos

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,10 +1,17 @@
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase-config";
 
 
-export const startNewNote = ()=>{
+export const startNewNote = () => {
 
-return ( dispatch, getState)=>{
-    const {uid} = getState().auth;
-    console.log(uid)
-}
+    return async (dispatch, getState) => {
+        const { uid } = getState().auth;
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        }
+        await setDoc(doc(collection(db, `${uid}`, "JOURNAL", 'NOTES')), newNote);
+    }
 
 }
