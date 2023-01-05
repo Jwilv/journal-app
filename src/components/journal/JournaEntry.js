0206bad37fbe2c8eb1c5806438ea42603681af7a
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-05 20:39:26 -0300

    se agregaron las props

--- src/components/journal/JournaEntry.js
+++ src/components/journal/JournaEntry.js
@@ -1,18 +1,25 @@
 import React from 'react'
 
-export const JournaEntry = () => {
+export const JournaEntry = ({ id, title, body, url }) => {
     return (
         <div className='journal__entry pointer'>
-            <div
-                className='journal__entry-picture'
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundImage: 'url(https://img.freepik.com/premium-photo/astronaut-outer-open-space-planet-earth-stars-provide-background-erforming-space-planet-earth-sunrise-sunset-our-home-iss-elements-this-image-furnished-by-nasa_150455-16829.jpg?w=2000)',
-                }}
-            ></div>
+            {
+                (url) &&
+                <div
+                    className='journal__entry-picture'
+                    style={{
+                        backgroundSize: 'cover',
+                        backgroundImage: `url(${url})`,
+                    }}
+                ></div>
+            }
             <div className='journal__entry-body'>
-                <p className='journal__entry-title'> Muchachooooooooooos</p>
-                <p className='journal__entry-content'>otra vez me volvi a ilucionar, quiero ganar la tercera, quiero ser campeon mundial</p>
+                <p className='journal__entry-title'>
+                    {title}
+                </p>
+                <p className='journal__entry-content'>
+                    {body}
+                </p>
             </div>
             <div className='journal__entry-date-box'>
                 <span>Monday</span>
