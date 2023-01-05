1a12aa33b799630e09456d582491f5f691a469fc
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-05 20:33:00 -0300

    se extrajeron als notes

--- src/components/journal/JournalEntries.js
+++ src/components/journal/JournalEntries.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { JournaEntry } from './JournaEntry'
 
 export const JournalEntries = () => {
 
-  const entries = [1, 2, 3, 4]
+  const {notes} = useSelector(state => state.notes);
 
   return (
     <div className='journal__entries'>
 
       {
-        entries.map( value => (
-          <JournaEntry key={value} /> 
+        notes.map( note => (
+          <JournaEntry 
+          key={note.id} 
+          {...note}
+          /> 
         ))
       }
 
