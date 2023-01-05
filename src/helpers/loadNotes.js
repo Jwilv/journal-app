d68d0ef56ff4b9af566d81fe2204fb44154944ae
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-05 16:55:42 -0300

    loadNotes

---
+++ src/helpers/loadNotes.js
@@ -0,0 +1,10 @@
+import { async } from "@firebase/util";
+import { collection, getDoc } from "firebase/firestore"
+import { db } from "../firebase/firebase-config"
+
+
+export const loadNotes = async()=>{
+    const docRef = collection(db,`${uid}`, 'journal', 'notes');
+    const notesSnap = await getDoc(docRef);
+    console.log(notesSnap);
+}
