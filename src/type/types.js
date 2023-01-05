8d38df8dc93778fbbf58f8d53ac0ce35db43d4a4
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-04 23:27:59 -0300

    se agregaron types

--- src/type/types.js
+++ src/type/types.js
@@ -5,6 +5,16 @@ export const types ={
 
     uiSetError : '[UI] Set Error',
     uiRemoveError : '[UI] Remove Error',
+
     uiStartLoading: '[UI] Start loading',
     uiFinishLoading: '[UI] Finish loading',
+
+    notesAddNewEntry: '[notes] New note',
+    notesActive: '[notes] Set active note',
+    notesLoad: '[notes] Load note',
+    notesUpdate: '[notes] Set update note',
+    notesFileUrl: '[notes] Update image url',
+    notesDelete: '[notes] Delete note',
+
+
 }
