525c9afaa90fcb75dd4f5fba9fb1899574195559
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-05 21:47:50 -0300

    se agregaron los valores

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useForm } from '../../hooks/useForm'
 import { NotesAppbar } from './NotesAppbar'
 
 export const NoteScreen = () => {
+
+    const { active: note } = useSelector(state => state.notes)
+    const [values, handleInputChanGet] = useForm(note)
+    const { title, body, url } = values;
+
     return (
         <div className='note__main-content'>
             <NotesAppbar />
@@ -11,14 +18,24 @@ export const NoteScreen = () => {
                     placeholder={'Title'}
                     className={'note__title-input'}
                     autoComplete={'off'}
+                    name={title}
+                    value={title}
+                    onChange={handleInputChanGet}
                 />
                 <textarea
-                placeholder='What happened today ?'
-                className={'note__textarea'}></textarea>
-                <div className='notes__images'>
-                    <img src='https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg'
-                    alt='img'/>
-                </div>
+                    placeholder='What happened today ?'
+                    className={'note__textarea'}>
+                </textarea>
+
+                {
+                    (url)
+                    && (
+                        <div className='notes__images'>
+                            <img src='https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg'
+                                alt='img' />
+                        </div>
+                    )
+                }
             </div>
 
         </div>
