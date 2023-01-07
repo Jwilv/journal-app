a98a456c0bd9b806fc9f60e96fc6c31b3e05836b
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-07 19:46:40 -0300

    se cambio por HashRouter

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppRouter } from './routers/AppRouter'
 import { store } from './store/store'
 
 export const JournalApp = () => {
     return (
         <Provider store={store}>
-            <BrowserRouter >
+            <HashRouter >
                 <AppRouter />
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     )
 }
