4298165439dcc3f7573a2fcd0c340371e7e10dbb
Wilvers Juan <juan.ignacio.wilvers@gmail.com>
2023-01-07 01:46:23 -0300

    se agergo agregar img

--- src/components/notes/NotesAppbar.js
+++ src/components/notes/NotesAppbar.js
@@ -6,21 +6,39 @@ export const NotesAppbar = () => {
 
     const dispatch = useDispatch();
 
-    const {active:note} = useSelector(state => state.notes)
+    const { active: note } = useSelector(state => state.notes)
 
-    const handleSave = ()=>{
-        dispatch( startSaveNote(note) )
+    const handleSave = () => {
+        dispatch(startSaveNote(note))
+    }
+    const handlePictureClick = () => {
+        document.querySelector('#fileSelector').click();
+    }
+    const handleFileChange = (event) => {
+        const file = event.target.files[0]
+        if(file){
+            dispatch();
+        }
     }
     return (
         <div className='notes__appbar'>
             <span>otra vez en primera van a ver a papa</span>
+            <input
+                id={'fileSelector'}
+                type={'file'}
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+            />
             <div>
-                <button className='btn'>
+                <button
+                    className='btn'
+                    onClick={handlePictureClick}
+                >
                     Picture
                 </button>
-                <button 
-                className='btn'
-                onClick={handleSave}
+                <button
+                    className='btn'
+                    onClick={handleSave}
                 >
                     Save
                 </button>
